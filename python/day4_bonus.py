c376a5f6b401f49f9bc35e03307fe10b9d5a1078
Michael H <michael@michaelhall.tech>
2023-12-04 10:14:44 -0500

    .

--- python/day4_bonus.py
+++ python/day4_bonus.py
@@ -1,36 +1,10 @@
 # an improvement on my original solution that avoids an issue of
 # ever holding too gigantic a list in memory
-from collections.abc import Iterable
 from pathlib import Path
-from queue import SimpleQueue
 
 path = Path(__file__).parent.with_name("inputs") / "day4.txt"
 
-
 def solver() -> tuple[int, int]:
-    with path.open(mode="r", encoding="utf-8") as fp:
-        scratchcard = fp.read().splitlines()
-
-    card_games: dict[int, int] = {}
-    for gamenum, line in enumerate(scratchcard, 1):
-        _game_header, game_data = line.split(":")
-        winners, ours = ({n for n in x.split(" ") if n} for x in game_data.split("|"))
-        card_games[gamenum] = len(winners & ours)
-
-    points = sum(1 << our_wins - 1 for our_wins in card_games.values() if our_wins)
-
-    all_cards_queue: SimpleQueue[Iterable[int]] = SimpleQueue()
-    all_cards_queue.put(card_games.keys())
-    card_count = 0
-    while not all_cards_queue.empty():
-        card_iter = all_cards_queue.get()
-        for card in card_iter:
-            card_count += 1
-            all_cards_queue.put(range(1 + card, 1 + card + card_games.get(card, 0)))
-    return points, card_count
-
-
-def solver2() -> tuple[int, int]:
     with path.open(mode="r", encoding="utf-8") as fp:
         scratchcard = fp.read().splitlines()
 
@@ -51,4 +25,4 @@ def solver2() -> tuple[int, int]:
     return points, sum(card_games)
 
 if __name__ == "__main__":
-    print(solver(), solver2())
+    print(solver())
